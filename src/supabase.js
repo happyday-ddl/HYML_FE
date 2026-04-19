473efc931909902467d2922b78fb53174fa7257e
Yannis Smith <60013922+Ywreckz@users.noreply.github.com>
2026-04-19 00:16:46 -0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl  = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnon = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnon);
+export const supabase = createClient(
+  'https://zkvxnsyktfwvphnzvtvo.supabase.co',
+  'sb_publishable_G2Ms6QbytngqIWVkG6wyiA_DANvxvUa'
+)
